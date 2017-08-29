69e294753184857ca85f6fa7bb627f9f37ee8613
Richard Harrah <topplethenunnery@gmail.com>
2017-08-28 22:59:15 -0400

    feat(versionology-core): adds stub versionology-core module
    affects: versionology-core

---
+++ packages/versionology-core/src/index.js
@@ -0,0 +1,10 @@
+// @flow
+
+/**
+ * This function says hello.
+ * @param {string} name Some name to say hello for.
+ * @return {string} The hello.
+ */
+const sayHello = (name: string = 'Nunnery'): string => `Hello, ${name}!`;
+
+export default sayHello;
